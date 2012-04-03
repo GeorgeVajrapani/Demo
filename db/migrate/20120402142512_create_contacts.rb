e857b36bba9d167c8fc582baec13db67a21f0a8b
Jorge <jocasi27@gmail.com>
2012-04-03 09:01:21 -0500

    Se crearon check buttonpara complementar la aplicacion

--- db/migrate/20120402142512_create_contacts.rb
+++ db/migrate/20120402142512_create_contacts.rb
@@ -4,6 +4,9 @@ class CreateContacts < ActiveRecord::Migration
       t.string :name
       t.datetime :birthdate
 	  t.string :sex
+	  t.boolean :languages_english
+	  t.boolean :languages_spanish
+	  t.boolean :languages_german
       t.string :email
       t.string :street_number
       t.string :colony
